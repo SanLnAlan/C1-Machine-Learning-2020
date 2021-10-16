e3928a116db7844c45ef6dcdccca2ec9e7287a93
Alan Sandoval <sandoval.leon.alan@gmail.com>
2021-10-15 22:47:47 -0500

    advance of postwork2

--- Sesion-02/Postwork2/MLUtilites.py
+++ Sesion-02/Postwork2/MLUtilites.py
@@ -1,16 +1,9 @@
-# • Funciones de evaluación con matriz de confusión.
-# • Funciones de obtención de Precisión (Accuracy), Sensibilidad y Especificidad.
-# • Funciones que comparen dos clasificadores: 
-#     ◦ Obtengas precisión, sensibilidad y especificidad del clasificador 1
-#     ◦ Obtengas precisión, sensibilidad y especificidad del clasificador 2
-#     ◦ Digas cual es mejor en terminos de precisión
-#     ◦ Digas cual es mejor en términos de sensibilidad
-#     ◦ Digas cual es mejor en términos de especificidad.
 # • Funciones de evaluación multiclase. 
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,LeaveOneOut,KFold
+from sklearn.metrics import confusion_matrix
 
 # • Funciones de separación de entrenamiento, validación y prueba. 
 def train_validation_test(x, y, porcentaje_entrenamiento, porcentaje_validacion, porcentaje_prueba):
@@ -33,7 +26,7 @@ def train_validation_test(x, y, porcentaje_entrenamiento, porcentaje_validacion,
     Returno:
     ----------
 
-    ejemplo
+    ejemplo:
     ----------
   
         >>> from sklearn import datasets
@@ -55,18 +48,42 @@ def train_validation_test(x, y, porcentaje_entrenamiento, porcentaje_validacion,
 
 
 # • Funciones de separación de datasets con K-Fold (el usuario debe poner el K, si K = 1 debe generar un Leave-One-Out Cross Validation).
-def separation_k_fold(data,K,):
-    
-    data = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+def separation_k_fold(data,K,aleatorio=True):
+    """
+    ejemplo:
+    -----------
+    >>> train, test = ML.separation_k_fold(data,4,True)
+    """
+    if K == 1:
+        loo = LeaveOneOut()
+        for train, test in loo.split(data):
+            print("%s %s" % (train, test))
+            return train, test
+    else:
+        kfold = KFold(K, aleatorio)
+        ciclo = 1
+        train = []
+        test = []
+        for indices_train, indices_test in kfold.split(data):
+            print("Ciclo: "+str(ciclo))
+            print("\t datos para entrenamiento:"+str(data[indices_train]))
+            print("\t datos para prueba:"+str(data[indices_test]))
+            train.append(data[indices_train])
+            test.append(data[indices_test])
+            ciclo+=1
+        return train, test
 
-    K = 4
-    # random_seed = 48
-    aleatorio = True
-    
-    kfold = KFold(K, aleatorio, random_seed)
-    ciclo = 1
-    for indices_train, indices_test in kfold.split(data):
-        print("Ciclo: "+str(ciclo))
-        print("\t datos para entrenamiento:"+str(data[indices_train]))
-        print("\t datos para prueba:"+str(data[indices_test]))
-        ciclo+=1
+
+# • Funciones de evaluación con matriz de confusión.
+def matrix_confusion(y_esperada,y_predicha):
+    result = confusion_matrix(y_esperada,y_predicha)
+
+# • Funciones de obtención de Precisión (Accuracy), Sensibilidad y Especificidad.
+
+
+# • Funciones que comparen dos clasificadores: 
+#     ◦ Obtengas precisión, sensibilidad y especificidad del clasificador 1
+#     ◦ Obtengas precisión, sensibilidad y especificidad del clasificador 2
+#     ◦ Digas cual es mejor en terminos de precisión
+#     ◦ Digas cual es mejor en términos de sensibilidad
+#     ◦ Digas cual es mejor en términos de especificidad.
