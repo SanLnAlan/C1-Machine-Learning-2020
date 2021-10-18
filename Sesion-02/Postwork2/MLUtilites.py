bba8bb683eccbc81c6ec63097f93e10919070438
Alan Sandoval <sandoval.leon.alan@gmail.com>
2021-10-17 20:05:50 -0500

    p2 done

--- Sesion-02/Postwork2/MLUtilites.py
+++ Sesion-02/Postwork2/MLUtilites.py
@@ -1,9 +1,11 @@
-# • Funciones de evaluación multiclase. 
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split,LeaveOneOut,KFold
 from sklearn.metrics import confusion_matrix
+from sklearn import datasets
+from sklearn.multiclass import OneVsRestClassifier,OneVsOneClassifier,OutputCodeClassifier
+from sklearn.svm import LinearSVC
+
 
 # • Funciones de separación de entrenamiento, validación y prueba. 
 def train_validation_test(x, y, porcentaje_entrenamiento, porcentaje_validacion, porcentaje_prueba):
@@ -13,18 +15,21 @@ def train_validation_test(x, y, porcentaje_entrenamiento, porcentaje_validacion,
     argumentos:
     ----------
 
-    x: 
+    x: datos de entrada
 
-    y:
+    y: datos de salida
 
-    porcentaje_entrenamiento:
+    porcentaje_entrenamiento: porcentaje asignado para entrenamiento
 
-    porcentaje_validacion:
+    porcentaje_validacion: porcentaje asignado para validación
 
-    porcentaje_prueba:
+    porcentaje_prueba: porcentaje asignado para prueba
 
     Returno:
     ----------
+    
+    [x_entrenamiento, x_validacion, x_prueba, y_entrenamiento, y_validacion, y_prueba]
+
 
     ejemplo:
     ----------
@@ -46,24 +51,46 @@ def train_validation_test(x, y, porcentaje_entrenamiento, porcentaje_validacion,
         return [x_train, None, x_temp, y_train, None, y_temp]
     return [x_train, x_val, x_test, y_train, y_val, y_test]
 
-
+ 
 # • Funciones de separación de datasets con K-Fold (el usuario debe poner el K, si K = 1 debe generar un Leave-One-Out Cross Validation).
 def separation_k_fold(data,K,aleatorio=True):
     """
+    Función de separación de datasets utlizando la validación cruzada de K-Fold
+
+    argumentos:
+    ----------
+
+    data: dataset a trabajar
+
+    K: número de ciclos o divisiones del dataset
+
+    aleatorio: si es True, afecta el orden de los indices, el cual controla la aleatoridad de cada separación.
+
+    Returno:
+    -----------
+    train: segmento de entrenamiento del dataset
+    
+    test: segmento de prueba del dataset
+
     ejemplo:
     -----------
     >>> train, test = ML.separation_k_fold(data,4,True)
     """
+    train = []
+    test = []
     if K == 1:
         loo = LeaveOneOut()
-        for train, test in loo.split(data):
-            print("%s %s" % (train, test))
-            return train, test
+        loo.get_n_splits(data)
+        for trained, tested in loo.split(data):
+            # print("%s %s" % (train, test))
+            print(f'{trained} {tested}')
+            train.append(trained)
+            test.append(tested)
+        return train, test
     else:
         kfold = KFold(K, aleatorio)
         ciclo = 1
-        train = []
-        test = []
+        
         for indices_train, indices_test in kfold.split(data):
             print("Ciclo: "+str(ciclo))
             print("\t datos para entrenamiento:"+str(data[indices_train]))
@@ -75,16 +102,76 @@ def separation_k_fold(data,K,aleatorio=True):
 
 
 # • Funciones de evaluación con matriz de confusión.
-def matrix_confusion(y_esperada,y_predicha):
+def matrix_confusion(y_esperada,y_predicha, printed=False):
+    """
+    Función que obtiene la matriz de confusión mediante los datos binarios de los datos de salida predichos y esperados.
+
+    argumentos:
+    ----------
+
+    y_esperada: salida de datos esperados
+
+    y_predicha: salida de datos predichos
+
+    printed: valor booleano que si es True imprime la matriz de confusión
+
+    Returno:
+    -----------
+    TN: valores acertados negativos
+
+    FP: valores no acertados positivos
+
+    FN: valores no acertados negativos
+
+    TP: valores acertados positivos
+
+    ejemplo:
+    -----------
+    >>> TN, FP, FN, TP = ML.matrix_confusion(y_predichos,y_esperados)
+    """
     result = confusion_matrix(y_esperada,y_predicha)
-    [TN, FP, FN, TP] = result.ravel()
-    return [TN, FP, FN, TP]
+    TN, FP, FN, TP = result.ravel()
+    if printed:
+        print(f"matriz de confusión \n{result}")
+    return TN, FP, FN, TP
 
 # • Funciones de obtención de Precisión (Accuracy), Sensibilidad y Especificidad.
-def results_matrix_confusion(TN, FP, FN, TP):
-    accuracy = (TP / (TP + FP) ) * 100
+def results_matrix_confusion(TN, FP, FN, TP, printed=False):
+    """
+    Función que obtiene los valores de precisión, sensibilidad y especificidad mediante los valores obtenidos de la función matrix_confusion()
+
+    argumentos:
+    ----------
+
+    TN: valores acertados negativos
+
+    FP: valores no acertados positivos
+
+    FN: valores no acertados negativos
+
+    TP: valores acertados positivos
+
+    printed: valor booleano que si es True imprime los resultados
+
+    Returno:
+    -----------
+    accuracy: Precisión de la matriz de confusión
+
+    sensitivity: Sensibilidad de la matriz de confusión
+
+    specificity: Especificidad de la matriz de confusión
+
+    ejemplo:
+    -----------
+    >>> accuracy, sensitivity, specificity = ML.results_matrix_confusion(TN, FP, FN, TP)
+    """
+    accuracy = (TP + TN) / (TP + TN + FP + FN) * 100
     sensitivity = (TP / (TP + FN) ) * 100
     specificity = (TN / (TN + FP) ) * 100
+    if printed:
+        print(f'precisión: {accuracy:.3f}')
+        print(f'sensibilidad: {sensitivity:.3f}')
+        print(f'specificidad: {specificity:.3f}')
     return accuracy, sensitivity, specificity
 
 
@@ -95,25 +182,89 @@ def results_matrix_confusion(TN, FP, FN, TP):
 #     ◦ Digas cual es mejor en términos de sensibilidad
 #     ◦ Digas cual es mejor en términos de especificidad.
 
-def clasification_comparative(y_esperada1,y_predicha1,y_esperada2,y_predicha2):
+def clasification_comparative(y_esperada,y_predicha,y_esperada2,y_predicha2):
+    """
+    Función que compara dos clasificadores para determinar cual es mejor en cuanto a precisión, sensibilidad y especificidad.
+
+    argumentos:
+    ----------
+
+    y_esperada: salida de datos esperados del primer clasificador
+
+    y_predicha: salida de datos predichos del primer clasificador
+
+    y_esperada2: salida de datos esperados del segundo clasificador
+
+    y_predicha2: salida de datos predichos del segundo clasificador
+
+
+    Returno:
+    -----------
+    Imprime los resultados de las comparaciones realizadas
+
+    ejemplo:
+    -----------
+    >>> ML.clasification_comparative(y_esperados,y_predichos,y_esperados2,y_predichos2)
+    """
     accuracy = []
     sensitivity = []
     specificity = []
-    [TN, FP, FN, TP] = matrix_confusion(y_esperada1,y_predicha1)
-    accuracy[0], sensitivity[0], specificity[0] = results_matrix_confusion(TN, FP, FN, TP)
-    [TN, FP, FN, TP] = matrix_confusion(y_esperada2,y_predicha2)
-    accuracy[1], sensitivity[1], specificity[1] = results_matrix_confusion(TN, FP, FN, TP)
+    y_esperada = [y_esperada,y_esperada2]
+    y_predicha = [y_predicha,y_predicha2]
+    for i in range(2):
+        [TN, FP, FN, TP] = matrix_confusion(y_esperada[i],y_predicha[i])
+        a, se, sp = results_matrix_confusion(TN, FP, FN, TP)
+        accuracy.append(a)
+        sensitivity.append(se)
+        specificity.append(sp)
     if accuracy[0] > accuracy[1]:
-        print('Clasficador 1 es mejor que clasificador 2 en cuanto a precisión: {accuracy[0]} > {accuracy[1]}')
+        print(f'Clasificador 1 es mejor que clasificador 2 en cuanto a precisión: {accuracy[0]:.3f} > {accuracy[1]:.3f}')
     else:
-        print('Clasficador 2 es mejor que clasificador 1 en cuanto a precisión: {accuracy[1]} < {accuracy[0]}')
+        print(f'Clasificador 2 es mejor que clasificador 1 en cuanto a precisión: {accuracy[1]:.3f} > {accuracy[0]:.3f}')
 
     if sensitivity[0] > sensitivity[1]:
-        print('Clasficador 1 es mejor que clasificador 2 en cuanto a sensibilidad: {sensitivity[0]} > {sensitivity[1]}')
+        print(f'Clasificador 1 es mejor que clasificador 2 en cuanto a sensibilidad: {sensitivity[0]:.3f} > {sensitivity[1]:.3f}')
     else:
-        print('Clasficador 2 es mejor que clasificador 1 en cuanto a sensibilidad: {sensitivity[1]} < {sensitivity[0]}')
+        print(f'Clasificador 2 es mejor que clasificador 1 en cuanto a sensibilidad: {sensitivity[1]:.3f} > {sensitivity[0]:.3f}')
 
     if specificity[0] > specificity[1]:
-        print('Clasficador 1 es mejor que clasificador 2 en cuanto a specificidad: {specificity[0]} > {specificity[1]}')
+        print(f'Clasificador 1 es mejor que clasificador 2 en cuanto a especificidad: {specificity[0]} > {specificity[1]:.3f}')
     else:
-        print('Clasficador 2 es mejor que clasificador 1 en cuanto a specificidad: {specificity[1]} < {specificity[0]}')
+        print(f'Clasificador 2 es mejor que clasificador 1 en cuanto a especificidad: {specificity[1]} > {specificity[0]:.3f}')
+
+
+# • Funciones de evaluación multiclase. 
+def multiclass_test(X,y, multiclass_type="OneVsRest"):
+    """
+    Función que realiza una evaluación multiclase
+
+    argumentos:
+    ----------
+
+    X: datos de entrada
+
+    y: datos de salida
+
+    multiclass_type: "OneVsRest" o "OneVsOne" o "OutputCode"
+
+
+    Returno:
+    -----------
+    resultado: resultado obtenido de la evaluación multiclase
+
+    ejemplo:
+    -----------
+    >>> from sklearn import datasets
+    >>> X, y = datasets.load_iris(return_X_y=True)
+    >>> r = ML.multiclass_test(X,y,"OneVsOne")
+
+    """
+    if multiclass_type == "OneVsRest":
+        result = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X, y).predict(X)
+        return result
+    if multiclass_type == "OneVsOne":
+        result = OneVsOneClassifier(LinearSVC(random_state=0)).fit(X, y).predict(X)
+        return result
+    if multiclass_type == "OutputCode":
+        result =  OutputCodeClassifier(LinearSVC(random_state=0), code_size=2, random_state=0).fit(X,y).predict(X)
+        return result 
